16668125a7127653a884a9b8986c12001f9173cd
Rob Barry <rob.barry@wsj.com>
2026-02-15 17:43:18 -0500

    fix catapult description: click to charge, not space

--- client/js/roles.js
+++ client/js/roles.js
@@ -39,7 +39,7 @@ export const ROLES = [
     type: TYPE_CATAPULT,
     name: 'Catapult',
     icon: '\uD83E\uDEA8',
-    desc: 'Aim with the mouse. Hold Space to charge, release to launch a rock.',
+    desc: 'Aim with the mouse. Click and hold to charge, release to launch a rock. Space: rapid fire.',
     usesAim: true,
   },
 ];
